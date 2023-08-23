0f28415fc13120e1fe307ec1791dbce23a0dffd8
Pis4ni <pis4wrk@gmail.com>
2023-08-23 19:55:25 +0200

    First commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// ! RECUPERO GLI ELEMENTI DI IN TERESSE DAL DOM
+const days = document.getElementById('days')
+const hours = document.getElementById('hours')
+const minutes = document.getElementById('minutes')
+const seconds = document.getElementById('seconds')
+
+const date = new Date()
+
+const daysActual = now.getHours()
+const hoursActual = now.getHours()
+const minutesActual = now.getHours()
+const secondsActual = now.getHours()
+
+setInterval(function(){
+
+}1000);
